772dc05aa6c3d553236273e48db8b3d88b55dee3
Quang Phan <79438276+quangphantally@users.noreply.github.com>
2023-08-22 15:49:13 +0700

    fdsf

--- src/color-flipper/knockout/app.js
+++ src/color-flipper/knockout/app.js
@@ -1,15 +1,31 @@
-class PageViewModel {
-  randomNumber$ = [1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F'];
-  color$ = ko.observable('')
+// class PageViewModel2 {
+//   randomNumber$ = [1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F'];
+//   color$ = ko.observable('')
 
-  renderColor(){
-    console.log(111)
-    this.color$ = '';
-    for(let i = 0; i  < 6; i++){
-      this.color$ += this.randomNumber$[Math.floor(Math.random() * this.randomNumber$.length)]
+//   renderColor(){
+//     console.log(111)
+//     this.color$ = '';
+//     for(let i = 0; i  < 6; i++){
+//       this.color$ += this.randomNumber$[Math.floor(Math.random() * this.randomNumber$.length)]
+//     }
+//     document.querySelector('.color').textContent = '#' + this.color$;
+//     document.body.style.backgroundColor = '#' + this.color$;
+//   }
+// }
+
+class PageViewModel {
+  color$ = ko.observable('')
+  _randomNumber = [1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F'];
+
+
+  renderColor() {
+    let color = '';
+    for(let i = 0; i < 6; i++){
+      color += this._randomNumber[Math.floor(Math.random() * this._randomNumber.length)]
     }
-    document.querySelector('.color').textContent = '#' + this.color$;
-    document.body.style.backgroundColor = '#' + this.color$;
+
+    console.log(color);
+    this.color$('#' + color);
   }
 }
 
