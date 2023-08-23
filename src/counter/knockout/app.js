cba613b30c038a60763a5a2d2239c7d7ef304dac
Quang Phan <79438276+quangphantally@users.noreply.github.com>
2023-08-23 14:58:14 +0700

    html css review carousel

--- src/counter/knockout/app.js
+++ src/counter/knockout/app.js
@@ -1,19 +1,28 @@
-class PageViewModel{
-count$ = ko.observable(0);
+class PageViewModel {
+  count$ = ko.observable(0);
+  color = ko.computed(function(){
+    if(this.count$() > 0){
+      return 'green';
+    } else if( this.count$() < 0){
+      return 'red';
+    } else {
+      return '#222';
+    }
+  },this)
 
-increase(){
-  let curr = this.count$();
-  this.count$(curr+1);
-}
+  increase() {
+    let curr = this.count$();
+    this.count$(curr + 1);
+  }
 
-decrease(){
-  let curr = this.count$();
-  this.count$(curr-1);
-}
+  decrease() {
+    let curr = this.count$();
+    this.count$(curr - 1);
+  }
 
-reset(){
-this.count$(0);
-}
+  reset() {
+    this.count$(0);
+  }
 }
 
 ko.applyBindings(new PageViewModel())
